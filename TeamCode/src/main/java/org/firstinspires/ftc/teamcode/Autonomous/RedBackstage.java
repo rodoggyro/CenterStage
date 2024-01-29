f490e544a5ab20318e7746e8dc0177261b374cb0
Rowan Shenoy <rshenoy309@gmail.com>
2024-01-28 19:18:35 -0500

    Adds code to place purple pixel

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/RedBackstage.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/RedBackstage.java
@@ -5,7 +5,6 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.robot.Robot;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import org.firstinspires.ftc.teamcode.RobotClass;
 
@@ -13,39 +12,81 @@ import org.firstinspires.ftc.teamcode.RobotClass;
 public class RedBackstage extends LinearOpMode {
     //Instantiate robot class
     RobotClass teamBot = new RobotClass(this);
-    
-    int parkingPosition  = 1;
-    
+
+    private int posOfTag;
     public void runOpMode() throws InterruptedException {
         //initialize robot
+
         teamBot.init(hardwareMap);
+        //posOfTag = teamBot.findTeamProp(502);
 
         waitForStart();
-    
-        teamBot.moveStraightWithEncoders(0.6, 82);
-        RobotClass.Position posOfTag = teamBot.findTeamProp();
-        
 
-        if(posOfTag == RobotClass.Position.LEFT){
-            teamBot.moveStraightWithEncoders(0.25, -10);
-            teamBot.gyroTurning(90);
-            teamBot.moveStraightWithEncoders(0.25, 15);
-
-        }else if(posOfTag == RobotClass.Position.CENTER){
-//            teamBot.moveStraightWithEncoders(0.6,10);
-        }else if(posOfTag == RobotClass.Position.RIGHT){
-            teamBot.moveStraightWithEncoders(0.25, -10);
+        if(posOfTag == 1){
+            teamBot.moveStraightWithEncoders(0.6, 20);
+            sleep(500);
             teamBot.gyroTurning(-90);
-            teamBot.moveStraightWithEncoders(0.25, 15);
-            teamBot.moveStraightWithEncoders(0.25, -20);
-            if(parkingPosition == 1){
-                teamBot.strafing(RobotClass.Direction.LEFT, 0.5, 1750);
-                teamBot.moveStraightWithEncoders(0.5, 100);
-            }
+            sleep(500);
+            teamBot.strafing(RobotClass.Direction.RIGHT, 0.5, 1000);
+            sleep(500);
+            teamBot.moveStraightWithEncoders(-0.6,10);
+            sleep(500);
+            //Open Purple Pixel Claw Side
+            //Shooting Purple Pixel onto SpikeMark with Intake
+            teamBot.strafing(RobotClass.Direction.LEFT, 0.6, 1000);
+            sleep(500);
+            teamBot.gyroTurning(180);
+            sleep(500);
+            teamBot.strafing(RobotClass.Direction.RIGHT, 0.5, 500);
+            sleep(500);
+            teamBot.moveStraightWithEncoders(0.6,100);
+            sleep(500);
+            //Open Yellow Pixel Claw Side
+            //Shooting Yellow Pixel in Backstage
+            teamBot.moveStraightWithEncoders(0.5,10);
+            sleep(500);
+
+        }else if(posOfTag == 2){
+            teamBot.moveStraightWithEncoders(0.6,42);
+            sleep(500);
+            //Open Purple Pixel Claw Side
+            //Shooting Purple Pixel onto SpikeMark with Intake
+            teamBot.moveStraightWithEncoders(-0.6,40);
+            sleep(500);
+            teamBot.gyroTurning(90);
+            sleep(500);
+            teamBot.moveStraightWithEncoders(0.6,100);
+            sleep(500);
+            //Open Yellow Pixel Claw Side
+            //Shooting Yellow Pixel into Backstage
+            teamBot.moveStraightWithEncoders(0.5,10);
+            sleep(500);
+
+        }else if(posOfTag == 3){
+            teamBot.moveStraightWithEncoders(0.6,26);
+            sleep(500);
+            teamBot.gyroTurning(90);
+            sleep(500);
+            teamBot.strafing(RobotClass.Direction.LEFT, 0.5, 1000);
+            sleep(500);
+            teamBot.moveStraightWithEncoders(-0.6,10);
+            sleep(500);
+            //Open Purple Pixel Claw Side
+            //Shooting Purple Pixel onto SpikeMark with Intake
+            teamBot.strafing(RobotClass.Direction.RIGHT, 0.6, 1500);
+            sleep(500);
+            teamBot.moveStraightWithEncoders(-0.6,100);
+            sleep(500);
+            //Open Yellow Pixel Claw Side
+            //Shooting Yellow Pixel into Backstage
+            teamBot.moveStraightWithEncoders(0.5,10);
+            sleep(500);
+
         }else{
             teamBot.moveStraightWithEncoders(0.25,-98);
             //Intake shoot
             teamBot.strafing(RobotClass.Direction.RIGHT,0.5,3000);
         }
+
     }
 }
