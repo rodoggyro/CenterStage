a5b9302fd80a213aee708003bd7b022c1eb6b830
Rowan Shenoy <rshenoy309@gmail.com>
2024-01-30 16:47:31 -0500

    blue side done

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/BlueAudience.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/BlueAudience.java
@@ -14,7 +14,7 @@ public class BlueAudience extends LinearOpMode {
     //Instantiate robot class
     RobotClass teamBot = new RobotClass(this);
     
-    int parkingPosition  = 1;
+    int parkingPosition  = 2;
     
     public void runOpMode() throws InterruptedException {
         //initialize robot
@@ -36,16 +36,25 @@ public class BlueAudience extends LinearOpMode {
                 teamBot.moveStraightWithEncoders(0.5, 60);
                 teamBot.gyroTurning(-90);
                 teamBot.moveStraightWithEncoders(0.5, -255);
+            } else if (parkingPosition == 2) {
+                teamBot.moveStraightWithEncoders(0.5, -65);
+                teamBot.gyroTurning(-90);
+                teamBot.moveStraightWithEncoders(1, -255);
             }
-            
+    
         }else if(posOfTag == RobotClass.Position.CENTER){
-            teamBot.moveStraightWithEncoders(0.6,50);
-            teamBot.gyroTurning(180);
-            teamBot.moveStraightWithEncoders( 0.5, 25);
-            if(parkingPosition == 1){
+            if (parkingPosition == 1) {
+                teamBot.moveStraightWithEncoders(0.6, 50);
+                teamBot.gyroTurning(180);
+                teamBot.moveStraightWithEncoders(0.5, 25);
+    
                 teamBot.moveStraightWithEncoders(0.5, -30);
                 teamBot.gyroTurning(-90);
                 teamBot.moveStraightWithEncoders(1, -255);
+            } else if (parkingPosition ==2) {
+                teamBot.moveStraightWithEncoders( 0.5, -65);
+                teamBot.gyroTurning(-90);
+                teamBot.moveStraightWithEncoders(0.5, -255);
             }
         }else if(posOfTag == RobotClass.Position.RIGHT){
             teamBot.moveStraightWithEncoders(0.25, -10);
@@ -55,6 +64,12 @@ public class BlueAudience extends LinearOpMode {
             if(parkingPosition == 1){
                 teamBot.gyroTurning(0);
                 teamBot.moveStraightWithEncoders(0.5, 65);
+                teamBot.gyroTurning(-90);
+                teamBot.moveStraightWithEncoders(1, -255);
+            } else if (parkingPosition == 2) {
+                teamBot.gyroTurning(0);
+                teamBot.strafing(RobotClass.Direction.RIGHT, 0.5, 250);
+                teamBot.moveStraightWithEncoders(0.5, -55);
                 teamBot.gyroTurning(-90);
                 teamBot.moveStraightWithEncoders(1, -255);
             }
