63e7471ffc9fee33c1d3bbed3f5eea51cc185121
Rowan Shenoy <rshenoy309@gmail.com>
2024-03-02 13:34:08 -0500

    done

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/BlueAudience.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/BlueAudience.java
@@ -24,20 +24,31 @@ public class BlueAudience extends LinearOpMode {
         
         teamBot.moveStraightWithEncoders(0.6, 72);
         RobotClass.Position posOfTag = teamBot.findTeamProp();
+        teamBot.clawRotator.setPosition(0.25);
         
         if(posOfTag == RobotClass.Position.LEFT){
             teamBot.gyroTurning(90);
-            teamBot.moveStraightWithEncoders(0.25, 15);
-            teamBot.moveStraightWithEncoders(-0.5, -20);
+            teamBot.moveStraightWithEncoders(0.25, 18);
+            teamBot.moveStraightWithEncoders(0.5, -20);
             teamBot.gyroTurning(0);
-    
+            teamBot.moveStraightWithEncoders(1, 60);
+            teamBot.gyroTurning(90);
+            teamBot.moveWithoutEncoders(1,1,2000);
         }else if(posOfTag == RobotClass.Position.CENTER){
+            teamBot.moveStraightWithEncoders(0.5, 50);
+            teamBot.gyroTurning(180);
             teamBot.moveStraightWithEncoders(0.5, 10);
+            teamBot.moveStraightWithEncoders(0.5, -15);
+            teamBot.gyroTurning(-90);
+            teamBot.moveWithoutEncoders(-1,-1, 2000);
         }else if(posOfTag == RobotClass.Position.RIGHT){
             teamBot.gyroTurning(-90);
-            teamBot.moveStraightWithEncoders(0.25, 10);
-            teamBot.moveStraightWithEncoders(0.25, -15);
-            
+            teamBot.moveStraightWithEncoders(0.25, 15);
+            teamBot.moveStraightWithEncoders(0.5, -18);
+            teamBot.gyroTurning(0);
+            teamBot.moveStraightWithEncoders(1, 60);
+            teamBot.gyroTurning(90);
+            teamBot.moveWithoutEncoders(-1,-1,2000);
         }
     }
 }
